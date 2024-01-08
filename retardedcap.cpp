69ba6dea27891b43c8698bc24171767ec6b88910
healthystrongmuslim <146497620+healthystrongmuslim@users.noreply.github.com>
2024-01-08 17:58:59 +0500

    retardedcap.cpp forces i small, L cap
    as 'I' (capital 'i') and 'l' (small 'L') often can't be distinguished, the code now forces both to the clearer case.

--- retardedcap.cpp
+++ retardedcap.cpp
@@ -9,6 +9,6 @@ int main(){
     string s; int i=0; unsigned st=time(0); srand(st);
     cout<<"Enter string:";
     getline(cin, s);
-    while(s[i]) cout<< char( isalpha(s[i])? isupper(s[i])? s[i++]+32*(rand()%2) : s[i++]-32*(rand()%2) : s[i++]); cout<<endl;
+    while(s[i]) cout<< char( isalpha(s[i])? (s[i]=='i' || s[i]=='I')? s[i++]='i':(s[i]=='l' || s[i]=='L')? s[i++]='L': isupper(s[i])? s[i++]+32*(rand()%2) : s[i++]-32*(rand()%2) : s[i++]); cout<<endl;
 }
 
