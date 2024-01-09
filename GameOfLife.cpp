753e4ed6277746ca9c0c804284383cb5ca5cd862
healthystrongmuslim <146497620+healthystrongmuslim@users.noreply.github.com>
2024-01-09 13:26:58 +0500

    delay-pick, err-handle, time-track, cls()

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -26,7 +26,10 @@ bool rondchkr(int, int, bool**);
 void update(bool**);
 void dltarr(bool**);
 
-int main(){
+int main(){long double delay;char cont='y';
+				cout<<"Enter board size and delay (seconds) :\t\n(default to 9 and 0.06;\tEnter 'y's to continue from default.)";
+				try{cin>>x;if(cin.fail() || x<1) throw 1;cin>>delay; if(cin.fail() || delay<0) throw 0;}
+				catch(int n){if(n)x=9; delay=0.06; cin.clear();}
 				cout<<"Enter board size:\t";
 				cin>>x;
 				int i=0, j=0; unsigned st=time(0); srand(st);
@@ -35,20 +38,25 @@ int main(){
 				// assign values to allocated memory
 								for (i=0;i<x;i++)
 												for (j=0;j<x; bord[i][j++]=rand()%2);
-				char cont='y';
-				while(cont=='y'){
-								plswait(0.1);
-								disp(bord);
-								cout<<"Enter 'y' n times for n iterations, Enter other key to quit:\t";
+				//				disp(bord);
+				std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
+			while(cont=='y'){
+								plswait(delay);
+								disp(bord); cout<<"Enter 'y' n times for n iterations, Enter other key to quit:\t";
 								cin>>cont;
 								update(bord);
 				}
+				std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
+				std::chrono::duration<long double, milli> time_put=t2-t1;
+				cout << "\n\33[1;44mRun took:\t\t" << time_put.count() << " milliseconds.\33[0m\n";
+//				disp(bord);
+				//if line 45 (full animation print) is commented, uncomment line 52 and 41 (print initial and final state only), and vice versa
 				dltarr(bord);
 }
 
 
 void disp(bool** A){
-				system("clear");
+				cls();
 				for(int i=0; i<x; cout<<i++<<endl)
 								for(int j=0; j<x; cout<<"\33["<<((A[i][j++])?41:104)<<"m   \33[0m");//41 for red dead cells, 104 for bright blue living cells
 				for(int i=0; i<x; cout<<((i>9)? " ":"  ")<<i++);
