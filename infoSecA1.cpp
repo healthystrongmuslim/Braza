83f09c07b9385eae4039b3a3af85a72b1cf85da0
Muhammad Musaab <146497620+healthystrongmuslim@users.noreply.github.com>
2025-02-17 15:33:32 +0500

    Much better graphs, coloured columns
    Frequency grapher:
    frequency-sorted (highest frequencies at top),
    better-scaled,
    shorter (skip graphing the tons of singletons i.e. 1-occurrence substrings),
    multicoloured (same number of occurrences → same colour; improved readability)
    
    Columnising:
    Coloured, and correspondingly-coloured in the transposing function.

--- infoSecA1.cpp
+++ infoSecA1.cpp
@@ -3,9 +3,13 @@
 #include <print>
 #include <iostream>
 #include <unordered_map>
+#include <queue>
 //#include <termios.h>
+#include<sys/ioctl.h>
 #define map std::unordered_map
-using std::print,std::string;
+#define maxHeap std::priority_queue
+using std::print, std::string;
+
 template <class moddingclass,typename t>
 inline t mod(t n,moddingclass k){return (k>0)? n= n%k + k*(n%k<0): 0;}//Modulus corrects for negative numbers
 char upOrLo(char c){return (c&32)?'a':'A';}
@@ -16,19 +20,45 @@ void caesarShift(char* plaintext, size_t dataLen, int key)
     if(plaintext[i] and isalpha(plaintext[i]))
       plaintext[i]=mod(plaintext[i]-(upOrLo(plaintext[i]))+key,26)+upOrLo(plaintext[i]);
 }
+
+struct freqPair
+{
+  string c;
+  int i;
+  freqPair(string c, int i): c(c), i(i){}
+};
+struct CompareFreq
+{
+  bool operator()(freqPair const& j1, freqPair const& j2)
+  {
+    return j1.i < j2.i;
+  }
+};
 void graphFreqs(map<string, int> &freqs, string title)
 {
+  int total=0, precision=4,singles=0;
+  maxHeap<freqPair, std::vector<freqPair>, CompareFreq> max_heap;
   print("\e[32m{}gram Frequencies:\n",title);
-  int total=0;
-  for(auto &[c,i]: freqs) print("{0}:\e[93m{1}\e[32m, ", c, i), total+=i;
-  print("\n\e[mGraphed \e[2m(relative to total number of {}graphs)\e[m:\n",title);
-  for(auto &[c,i]: freqs) print("{0}\e[36m{1:->{2}}\e[m\n", c, i, int(i*100/total));
+  for(auto &[c,i]: freqs)
+  {
+    print("{0}:\e[93m{1}\e[32m, ", c, i);total+=i;
+    if(i>1) max_heap.push(freqPair(c,i)); //avoid graphing singletons; singletons are not interesting
+    else singles++;
+  }
+  print("\n\e[mGraphed \e[2m(relative to total number of {}graphs ({}), and excluding ({}) singletons)\e[m:\n",title, total, singles);
+  for(; !max_heap.empty(); max_heap.pop())
+  {
+    freqPair fp=max_heap.top();
+    print("{0}\e[4{5}m{1:>{2}.{4}f}\e[;2;36m {3}\e[m\n",
+    fp.c, float(fp.i)/total, int(float(fp.i*500)/total)+precision+1, fp.i, precision,fp.i%7+1);
+  }
+  //c string, i occurences, i/total relative[-to-total] observed occurence rate, i*500/total graph width, precision
   print("\n");
 }
 
-void freqAnalysis(char* userData, const size_t dataLen)
+void freqAnalysis(char* userData, const size_t dataLen,string title="")
 {
-  print("\n\e[93mFrequency Analysis\e[m\n");
+  print("\n\e[93m{} Frequency Analysis\e[m\n",title);
   map<string, int> monoFreq,biFreq,triFreq;
   for(size_t i=0; i<=dataLen; i++)
   {
@@ -54,8 +84,8 @@ void columnised(char userData[],size_t dataLen,size_t cols,size_t padLen)
 {
   char padr='X';
   if(padLen) print("\t\e[2;33mMessage will be padded ({}) to match key…\e[m\n",padLen);
-  print("Message Columnised:\n\e[46m");
-  for(size_t i=0;i<=dataLen;) print("{}{}",userData[i++], (i%cols==0)?'\n':'\0');
+  print("Message Columnised:\n");
+  for(size_t i=0;i<=dataLen;i++) print("{}\e[;3{}m{}", (i%cols==0)?'\n':'\0', i%((cols<=7)?cols:7)+1, userData[i]);
   for(size_t i=0;i<padLen;i++) print("{}",padr);
   print("\e[m\n");
 }
@@ -64,22 +94,31 @@ void transposeInto(char userData[],char tpose[],size_t dataLen,size_t cols,size_
 {
   char padr='X';
   size_t rows=dataLen/cols +(padLen?1:0);
-  /*/
+  /*/ //For debugging
   static struct termios oldt, newt;
   tcgetattr(0, &oldt);
   newt = oldt; newt.c_lflag &= ~264;
   tcsetattr(0, 0, &newt);
-  getchar(), getchar();
+  getchar(), getchar(); //take blanks from enter key
   //*/
+  struct winsize w;
+  char garbage = ioctl(1, TIOCGWINSZ, &w);
+  int termWidth = w.ws_col;//window width, for matching column colors while printing like 
+  print("Transposing…\n");
   for(size_t i=0; i<dataLen+padLen; i++)
-    tpose[(i%cols)*rows+i/cols]=(i<dataLen)? userData[i]: padr;
-    //,print("\e[1K\e[G{}\t{}->{}",tpose, i, (i%cols)*rows+i/cols), getchar(); tcsetattr(0, 0, &oldt);
+  {
+    char cur=(i<dataLen)? userData[i]: padr;
+    tpose[(i%cols)*rows+i/cols]=cur;
+    print("\e[{}G\e[;3{}m{}{}",(i%cols)*rows+i/cols+1, i%((cols<=7)?cols:7)+1,cur, (i%rows==termWidth-1)?'\n':'\0');//
+    //print("\e[1K\e[G{}\t{}->{}",tpose, i, (i%cols)*rows+i/cols), getchar(); tcsetattr(0, 0, &oldt);
+  }
+  print("\e[m\n");
 }
 void detransing(char tpose[], char dpose[], size_t len, size_t cols)
 {
   size_t rows=len/cols;
   for(size_t i=0; i<len; i++)
-  dpose[(i%rows)*cols+i/rows]=tpose[i];//==dpose[i]=tpose[(i%cols)*rows+i/cols];
+    dpose[(i%rows)*cols+i/rows]=tpose[i];//==dpose[i]=tpose[(i%cols)*rows+i/cols];
 }
 
 int main(int argc, char** argv)
@@ -91,7 +130,7 @@ int main(int argc, char** argv)
   catch (std::invalid_argument &e){print("Invalid arg\n");exit(2);}
   catch (std::out_of_range &e){print("Out of range of int\n");exit(4);}
   catch (int err){print("Out of limit\tCorrecting…\n"); rno=mod(rno,rnoLimit);}
-  const size_t dataLen=50;
+  const size_t dataLen=500;
   char userData[dataLen+1]={0};
   print("Enter {}'s data ({} characters max):\t\e[33m", rno, dataLen);
   fgets(userData, dataLen+1, stdin);
@@ -103,14 +142,14 @@ int main(int argc, char** argv)
 Fixed, Plaintext is:", userData);
   for (size_t i=0; i<=dataLen;i++)
     if('\n'==userData[i] or (i>0 and 0==userData[i-1])) userData[i]=0;
-    else if(!isalpha(userData[i]) and userData[i]!=' ') userData[i]=mod(userData[i],26)+upOrLo(userData[i]);
+    else if(!isalpha(userData[i]) and userData[i]!=' ') userData[i]=' ';//mod(userData[i],26)+upOrLo(userData[i]);
   userData[dataLen]=0;
   int key=rno%26;
-  print("\"\e[44m{}\e[m\"\nKey: {}\n\
-Ciphertext:\"\e[46m", userData, key);
+  print("\"\e[44m{}\e[m\"\nKey: {}\n", userData, key);
+  freqAnalysis(userData,dataLen, "Original Data");
   //Caesar shifting
   caesarShift(userData, dataLen, key);
-  print("{}\e[m\"", userData);
+  print("Ciphertext:\"\e[46m{}\e[m\"", userData);
   freqAnalysis(userData,dataLen);
   
   //for(size_t i=0; i<dataLen; i++) print("{0:d}:\e[35m{0}\e[m ", userData[i]);
@@ -142,3 +181,10 @@ Ciphertext:\"\e[46m", userData, key);
   
   //columnised(transposed,dataLen+padLen,row);
 }
+/*A tad over 500 chars:
+Multiply saying void a make and fruit given own signs. Form fill hath every, i fowl were Have for living night multiply he their whales in had moved place had seas seasons you'll. Beginning day their make man earth You'll the image firmament. Were face god thing night firmament is heaven saying. Deep replenish, deep. Two were. Firmament moved shall fly man i one it, fly sea. Their fish us from created the, great divided saying brought she'd it face subdue man is creepeth together man were. Evening
+
+Another tad from the book "Probability & Statistics for Engineers & Scientists (Ninth Edition)" by Walpole, pg 110:
+As far as potential hazards with the use of material in this chapter, the warning to the reader is not to read more into the material than is evident. The general nature of the probability distribution for a specific scientific phenomenon is not obvious from what is learned in this chapter. The purpose of this chapter is for readers to learn how to manipulate a probability distribution, not to learn how to identify a specific type. Chapters 5 and 6 go a long way toward identification according to the general nature of the scientific system.
+
+*/
